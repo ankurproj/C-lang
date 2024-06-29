4434ee45ebe9939925f7ee459cc47b72884ef4ae
ANKUR KUMAR <155262565+ankurproj@users.noreply.github.com>
2024-06-30 01:19:28 +0530

    Add files via upload
    CH 1

---
+++ 1first.c
@@ -0,0 +1,10 @@
+// first c program
+#include <stdio.h>
+
+int main()
+{
+    int tom;
+    int Tom;
+    printf("Hello I am learning c with harry");
+    return 0;
+}
---
+++ i_love_you.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int i;
+
+    for(i = 1; i<= 320; i++){
+        printf("I LOVE YOU \3 \3 \3 \3 \3 \3");
+    }
+return 0;
+}
